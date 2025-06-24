5e3dfcc389b93046cc85ef1da4dae982509a5551
Yuva-galla <gallayuva.teja@cognizant.com>
2025-06-24 15:49:52 +0530

    Add files via upload

---
+++ online-exam-portal-gateway/src/main/java/com/onlineexam/gateway/OnlineExamPortalGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.onlineexam.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OnlineExamPortalGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamPortalGatewayApplication.class, args);
+	}
+
+}
---
+++ online-exam-portal-gateway/src/test/java/com/onlineexam/gateway/OnlineExamPortalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineexam.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamPortalGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
